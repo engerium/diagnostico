0b841553650b7b438a7e8852ab665dd781275cab
engerium <engerium@gmail.com>
2015-04-29 21:47:33 +0800

    JS rewrite to improve performance

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -22,7 +22,7 @@ $(function() {
 		hwaccel: true, // Whether to use hardware acceleration
 		className: 'spinner', // The CSS class to assign to the spinner
 		zIndex: 2e9, // The z-index (defaults to 2000000000)
-		top: '50%', // Top position relative to parent
+		top: '22%', // Top position relative to parent
 		left: '50%' // Left position relative to parent
 	};
 
@@ -32,8 +32,9 @@ $(function() {
 
 	$("#search-input").keyup(function(e) {
 		if(e.keyCode === 13) {
-			query();
 			$("#search-input").blur(); // Unfocus search input
+			spinner.spin(target);
+			query();
 		}
 	});
 
@@ -43,135 +44,25 @@ $(function() {
 // Query function
 function query() {
 	
+	// Remove any existing results
+	removeResults();
+
 	// Variables
-	var results;
-	var ip_results;
-	var ip_info;
 	var input = $("#search-input").val().trim();
-	var ipaddr;
-	var domain;
-	var name;
-	var organization;
-	var city;
-	var state;
-	var ip_longitude;
-	var ip_latitude;
-	var ip_asn;
-	var ip_city;
-	var ip_region;
-	var ip_country;
-	var ip_cc;
-	var ip_isp;
-	var ip_whois;
 
-
-
-	// Check if input is a valid IP address else check DNS
+	// If input is a valid IP address
 	if (validateIP(input)) {
-		ipaddr = input;
-		domain = getHost(input);
-		$(".sub-q").css({"background-color": "#43A047"});
 
-		// If domain is undefined
-		if (!domain) {
-			domain = "No domain found"
-			$(".sub-q").css({"background-color": "#FFB300"});
-		}
+		// Get domain name for input
+		getHost(input);
 
 	} else {
-		ipaddr = getIP(input);
-		domain = input;
 
-		if (validateIP(ipaddr)) {
+		// Get IP address for input
+		getIP(input);
 
-			try {
-				results = getWhois(domain)[0];
-				name = results.name;
-				organization = results.organization;
-				city = results.city;
-				state = results.state;
-				$(".sub-q").css({"background-color": "#43A047"});
-			} catch (e) {
-				name, organization, city, state = null;
-			}
-
-		} else {
-			ipaddr = "Unresolvable domain"
-			$(".sub-q").css({"background-color": "#D32F2F"});
-			$("#results-q").empty(); // Empty past q results
-			$("#results-d").empty(); // Empty past d results
-			$("#results-w").empty(); // Empty past d results
-			$(".sub-q").addClass("hidden"); // Make div hidden as originally declared
-			$(".sub-d").addClass("hidden"); // Make div hidden as originally declared
-			$(".sub-w").addClass("hidden"); // Make div hidden as originally declared
-		}
 	}
 
-	// Only get whois if there is a valid IP address
-	if (validateIP(ipaddr)) {
-
-		try {
-			ip_results = getWhois(ipaddr);
-			ip_info = ip_results[0];
-			ip_whois = ip_results[1].whois;
-		
-			ip_longitude = ip_info.longitude;
-			ip_latitude = ip_info.latitude;
-			ip_asn = ip_info.asn;
-			ip_city = ip_info.city;
-			ip_region = ip_info.region;
-			ip_country = ip_info.country;
-			ip_cc = ip_info.country_code3;
-			ip_isp = ip_info.isp;
-		} catch (e) {
-			ip_latitude, ip_longitude, ip_asn, ip_city, ip_region, ip_country, ip_cc, ip_isp, ip_whois = null;
-		}
-
-		console.log(ip_latitude, ip_longitude, ip_asn, ip_city, ip_region, ip_country, ip_cc, ip_isp, ip_whois);
-
-	} else {
-		ip_latitude, ip_longitude, ip_asn, ip_city, ip_region, ip_country, ip_cc, ip_isp, ip_whois = null;
-	}
-
-	$("#results-q").empty(); // Empty past q results
-	$("#results-d").empty(); // Empty past d results
-	$("#results-w").empty(); // Empty past d results
-	$(".sub-q").addClass("hidden"); // Make div hidden as originally declared
-	$(".sub-d").addClass("hidden"); // Make div hidden as originally declared
-	$(".sub-w").addClass("hidden"); // Make div hidden as originally declared
-
-	// Display queried IP and Domin
-	$("#results-q").append("<h1>" + ipaddr + "</h1>");
-	$("#results-q").append("<h2><i>" + domain + "</i></h2>");
-	$(".sub-q").removeClass("hidden"); // Show q results
-
-	// Display domain information if available
-	if (name && organization && city && state) {	
-		$("#results-d").append("<h2>" + name + "</h2>");
-		$("#results-d").append("<h3><i>" + organization + "</i></h3>");
-		$("#results-d").append("<h3>" + city + ", " + state + "</h3>");
-		$(".sub-d").removeClass("hidden"); // Show d results
-	}
-
-	// Display IP address information if available
-	if (validateIP(ipaddr)) {
-		$("#results-w").append("<h2>" + ip_asn + "</h2>");
-		$("#results-w").append("<h3><i>" + ip_isp + "</i></h3>");
-		$("#results-w").append("<h3>" + ip_country + "</h3>");
-		
-		if (ip_city) {
-			if (ip_region) {
-				$("#results-w").append("<h3>" + ip_city + ", " + ip_region + "</h3>");
-			} else {
-				$("#results-w").append("<h3>" + ip_city +"</h3>");
-			}
-		}
-
-		$(".sub-w").removeClass("hidden"); // Show w results
-	}
-
-	// Unfocus search input
-	$("#search-input").val("").blur();
 }
 
 
@@ -184,93 +75,319 @@ function validateIP(input) {
 }
 
 
-// Resolve IP address function
-function getIP(input) {
-	var returnVal;
-	$.ajax({
-		type: "GET",
-		url: diagnosticoFQDN + "api/getip/" + btoa(input),
-		datatype: "json",
-		async: false,
-		success: function(data) {
-			try {
-				returnVal = data.answer[0].rdata;
-			} catch (e) {
-				returnVal = null;
-			}
-			
-		},
-		error: function(data) {
-			console.log(data);
-		}
-	});
-
-	// If still not an IP address
-	if (!validateIP(returnVal)) {
-		$.ajax({
-			type: "GET",
-			url: diagnosticoFQDN + "api/getip/" + btoa(returnVal),
-			datatype: "json",
-			async: false,
-			success: function(data) {
-				try {
-					returnVal = data.answer[0].rdata;
-				} catch (e) {
-					returnVal = null;
-				}
-				
-			},
-			error: function(data) {
-				console.log(data);
-			}
-		});
-	}
-
-	return returnVal;
-}
-
 // Resolve domain function
 function getHost(input) {
-	var returnVal;
 	$.ajax({
 		type: "GET",
 		url: diagnosticoFQDN + "api/getdomain/" + btoa(input),
 		datatype: "json",
-		async: false,
 		success: function(data) {
 			try {
-				returnVal = data.answer[0].rdata;
+
+				// Get domain name from JSON
+				domain = data.answer[0].rdata;
+
+				// Set query box to green
+				$(".sub-q").css({"background-color": "#43A047"});
+
+				cl("[DEBUG] getHost() success try : " + domain); //Debug
+
+				// Get whois information for domain name
+				//getWhois(domain, "d");
+
+				// Get whois information for ip address
+				getWhois(input, "i");
+
 			} catch (e) {
-				returnVal = null;
+				// Set domain name to not found
+				domain = "No domain found";
+
+				// Set query box to yellow
+				$(".sub-q").css({"background-color": "#FFB300"});
+
+				cl("[DEBUG] getHost() success catch : " + domain); //Debug
+
+				// Get whois information for ip address
+				getWhois(input, "i");
 			}
+
+			cl("[DEBUG] getHost() success end : " + domain); //Debug
+
+			// Display query information
+			qDisplay(input, domain);
+
 		},
 		error: function(data) {
-			console.log(data);
+			// Print error message
+			cl("[ERROR] getHost() :\n" + data);
+		}
+	});
+}
+
+
+// Resolve IP address function
+function getIP(input) {
+	$.ajax({
+		type: "GET",
+		url: diagnosticoFQDN + "api/getip/" + btoa(input),
+		datatype: "json",
+		success: function(data) {
+			try {
+
+				// Get IP address from JSON
+				ipaddr = data.answer[0].rdata;
+
+				cl("[DEBUG] getIP() success try : " + ipaddr); //Debug
+
+				// Diplay query information
+				qDisplay(ipaddr, input);
+
+				// If still not an IP address
+				if (!validateIP(ipaddr)) {
+					$.ajax({
+						type: "GET",
+						url: diagnosticoFQDN + "api/getip/" + btoa(ipaddr),
+						datatype: "json",
+						success: function(data) {
+							try {
+
+								// Remove any existing results
+								removeResults();
+
+								// Get IP address from JSON
+								ipaddr = data.answer[0].rdata;
+
+								// Diplay query information
+								qDisplay(ipaddr, input);
+
+								// Get ip address information
+								getWhois(ipaddr, "i");
+
+							} catch (e) {
+								// Set IP address to null
+								ipaddr = null;
+
+								// Display query error
+								queryError("Unresolvable domain", input);
+
+							}
+							
+						},
+						error: function(data) {
+							// Print error message
+							cl("[ERROR] second getIP() :\n" + data);
+						}
+					});
+				}
+
+				// Get domain name information
+				getWhois(input, "d");
+
+				// If valid ip address
+				if (validateIP(ipaddr)) {
+					// Get ip address information
+					getWhois(ipaddr, "i");
+				}
+				
+
+
+			} catch (e) {
+
+				// Set IP address to null
+				ipaddr = null;
+
+				// Display query error
+				queryError("Unresolvable domain", input);
+
+			}
+			
+		},
+		error: function(data) {
+			// Print error message
+			cl("[ERROR] getIP() :\n" + data);
 		}
 	});
 
-	return returnVal;
+	
+
 }
 
+
 // Get whois information
-function getWhois(input) {
-	var returnVal;
+function getWhois(input, type) {
+	// Variables
+	var name, organization, city, state;
+	var ip_latitude, ip_longitude, ip_asn, ip_city, ip_region, ip_country, ip_cc, ip_isp;
+
 	$.ajax({
 		type: "GET",
 		url: diagnosticoFQDN + "api/whois/" + btoa(input),
 		datatype: "json",
-		async: false,
 		success: function(data) {
-			try {
-				returnVal = data;
-			} catch (e) {
-				returnVal = null;
+			// If domain type
+			if (type === "d") {
+
+				try {
+
+					// Get information from JSON
+					name = data[0].name;
+					organization = data[0].organization;
+					city = data[0].city;
+					state = data[0].state;
+
+					cl("[DEBUG] getWhois() try d : " + data); //Debug
+
+					// Display domain information
+					dDisplay(name, organization, city, state);
+
+				} catch (e) {
+					// Set variables to null
+					name, organization, city, state = null;
+					cl("[DEBUG] getWhois() catch d : " + data); //Debug
+				}
+
+			} else {
+
+				try {
+
+					// Get information from JSON
+					ip_info = data;
+					ip_longitude = ip_info.longitude;
+					ip_latitude = ip_info.latitude;
+					ip_asn = ip_info.asn;
+					ip_city = ip_info.city;
+					ip_region = ip_info.region;
+					ip_country = ip_info.country;
+					ip_cc = ip_info.country_code3;
+					ip_isp = ip_info.isp;
+
+					cl("[DEBUG] getWhois() try i : " + data); //Debug
+
+					// Display IP address information
+					iDisplay(ip_latitude, ip_longitude, ip_asn, ip_city, ip_region, ip_country, ip_cc, ip_isp);
+
+				} catch (e) {
+					// Set variables to null
+					ip_latitude, ip_longitude, ip_asn, ip_city, ip_region, ip_country, ip_cc, ip_isp = null;
+					cl("[DEBUG] getWhois() catch i : " + data); //Debug
+				}
+
 			}
+			
 		},
 		error: function(data) {
-			console.log(data);
+			// Print error message
+			cl("[ERROR] getWhois() :\n" + data);
 		}
 	});
+}
 
-	return returnVal;
+
+// Display query information function
+function qDisplay(ipaddr, domain) {
+
+	// Append IP address
+	$("#results-q").append("<h1>" + ipaddr + "</h1>");
+
+	// Append domain name
+	$("#results-q").append("<h2><i>" + domain + "</i></h2>");
+
+	// Remove hidden property
+	$(".sub-q").removeClass("hidden");
+
+}
+
+
+// Display domain information function
+function dDisplay(name, organization, city, state) {
+
+	// Append name
+	$("#results-d").append("<h2>" + name + "</h2>");
+
+	// Append organization
+	$("#results-d").append("<h3><i>" + organization + "</i></h3>");
+
+	// Append city and state
+	$("#results-d").append("<h3>" + city + ", " + state + "</h3>");
+
+	// Remove hidden property
+	$(".sub-d").removeClass("hidden");
+
+}
+
+function iDisplay(ip_latitude, ip_longitude, ip_asn, ip_city, ip_region, ip_country, ip_cc, ip_isp) {
+
+	// Append ASN number
+	$("#results-i").append("<h2>" + ip_asn + "</h2>");
+
+	// Append ISP
+	$("#results-i").append("<h3><i>" + ip_isp + "</i></h3>");
+
+	// Append country
+	$("#results-i").append("<h3>" + ip_country + "</h3>");
+
+	// If city is present
+	if (ip_city) {
+		// If region is present
+		if (ip_region) {
+			// Append city and region
+			$("#results-i").append("<h3>" + ip_city + ", " + ip_region + "</h3>");
+		} else {
+			// Append city
+			$("#results-i").append("<h3>" + ip_city +"</h3>");
+		}
+	}
+
+	// Remove hidden property
+	$(".sub-i").removeClass("hidden");
+
+	// Remove search input
+	$("#search-input").val("");
+
+	// Stop spinjs
+	spinner.stop();
+
+}
+
+
+// Display error for query function
+function queryError(ipaddr, domain) {
+
+	// Set red error background
+	$(".sub-q").css({"background-color": "#D32F2F"});
+
+	// Append IP address
+	$("#results-q").append("<h1>" + ipaddr + "</h1>");
+
+	// Append domain name
+	$("#results-q").append("<h2><i>" + domain + "</i></h2>");
+
+	// Remove hidden property
+	$(".sub-q").removeClass("hidden");
+
+	// Remove search input
+	$("#search-input").val("");
+	
+}
+
+
+// Remove results function
+function removeResults() {
+
+	$("#results-q").empty(); // Empty past q results
+	$("#results-d").empty(); // Empty past d results
+	$("#results-i").empty(); // Empty past d results
+	$(".sub-q").addClass("hidden"); // Make div hidden as originally declared
+	$(".sub-d").addClass("hidden"); // Make div hidden as originally declared
+	$(".sub-i").addClass("hidden"); // Make div hidden as originally declared
+	$(".sub-q").css({"background-color": "#43A047"}); // Reset css backgound color
+	$(".sub-d").css({"background-color": "#43A047"}); // Reset css backgound color
+	$(".sub-i").css({"background-color": "#43A047"}); // Reset css backgound color
+
+}
+
+
+// Debug console log function
+function cl(input) {
+	console.log(input);
 }
--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,6 @@
 class Api::ApiController < ApplicationController
+	default_timeout=10
+
 	def getdomain
 		domain = Base64.decode64(params[:ipaddr])
 		response = HTTParty.get("http://api.statdns.com/x/#{domain}")
@@ -20,12 +22,13 @@ class Api::ApiController < ApplicationController
 		if (IPAddress.valid? query)
 			geoip_response = HTTParty.get("http://www.telize.com/geoip/#{query}")
 
-			whois_json = {}
-			whois_json['whois'] = whois_response.to_s
-			whois_json = whois_json.to_json
-			concat_json = [geoip_response.body, whois_json].map{ |o| JSON[o] }.to_json
+			#whois_json = {}
+			#whois_json['whois'] = whois_response.to_s
+			#whois_json = whois_json.to_json
+			#concat_json = [geoip_response.body, whois_json].map{ |o| JSON[o] }.to_json
 
-			render json: concat_json
+			#render json: concat_json
+			render json: geoip_response.body
 		else
 			whois_parser = whois_response.parser
 
